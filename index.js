bc990a4a05a3d9b60b0fdadc6ae05d4b375b098d
thieuhuyhoang146 <115924361+thieuhuyhoang146@users.noreply.github.com>
2023-09-21 09:43:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+const noiDung = {
+    "Truong:": "UIT",
+    "DiaChi": "Thu Duc"
+};
+
+const noiDung2 = {
+    "KTX:": "DHQG HCM",
+    "DC": "Thu Duc",
+    "Khu": "Khu A, Khu B"
+};
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-app.listen(port, ()=>{
-    console.log('Server listening at https://localhost:${port}');
+    console.log('Sent information successfully');
+    res.send("Thieu Huy Hoang - 20521350 Xin Chao");
+})
+
+app.get('/truong', (req, res) => {
+    console.log('Sent information successfully');
+    res.send(noiDung);
+})
+
+app.get('/ktx', (req, res) => {
+    console.log('Sent information successfully');
+    res.send(noiDung2);
+})
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+    console.log(`I'm listening on port ${port}`);
 });
